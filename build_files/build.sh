e01ed292e14301fe8c3b67f5b540b7a02e72988c
sysxfml <61449338+sysxfml@users.noreply.github.com>
2026-07-30 15:42:48 +0800

    Update build script for kernel installation from GitHub
    Refactor build script to download and install kernel RPMs from GitHub releases, removing unnecessary comments and improving structure.

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-# dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-# systemctl enable podman.socket
+GITHUB_REPO="sysxfml/bazzite-amd-hdmi-kde-new"
+RELEASE_TAG="latest-kernel"
+API_URL="https://api.github.com/repos/${GITHUB_REPO}/releases/tags/${RELEASE_TAG}"
 
 pushd /usr/lib/kernel/install.d
 mv 05-rpmostree.install 05-rpmostree.install.bak
@@ -45,44 +27,32 @@ pkgs=(
     kernel-common
 )
 
-dnf5 -y remove --no-autoremove ${pkgs[@]}
+dnf5 -y remove --no-autoremove ${pkgs[@]} || true
 
-cat /ctx/KF?? > /tmp/kernel-split.rpm
+rm -rf /usr/lib/modules/*
 
-dnf5 --setopt=disable_excludes=* install -y /ctx/kernel-*.rpm /tmp/kernel-*.rpm
+mkdir -p /tmp/kernel-rpms
+pushd /tmp/kernel-rpms
+
+curl -s "${API_URL}" | grep "browser_download_url.*rpm" | cut -d '"' -f 4 | xargs -n 1 curl -L -O
+
+dnf5 --setopt=disable_excludes=* install -y ./*.rpm
+
+popd
+rm -rf /tmp/kernel-rpms
 
 pushd /usr/lib/kernel/install.d
 mv -f 05-rpmostree.install.bak 05-rpmostree.install
 mv -f 50-dracut.install.bak 50-dracut.install
 popd
 
-# If images already installed cliwrap, use it. Only used in transition period, so it should be removed when base images like Ublue remove cliwrap
-if [ -f "/usr/libexec/rpm-ostree/wrapped/dracut" ]; then
+if [ -f "/usr/libexec/rpm-ostree/wrapped/dracut" ]
+then
   DRACUT="/usr/libexec/rpm-ostree/wrapped/dracut"
 else
   DRACUT="/usr/bin/dracut"
 fi
 
-# NOTE!
-# This won't work when Fedora starts to utilize UKIs (Unified Kernel Images).
-# UKIs will contain kernel + initramfs + bootloader.
-# Refactor the module to support UKIs once they are starting to be used, if possible.
-# That won't be soon, so this module should work for good period of time
-
-kernel_count=0
-for kernel_path in /usr/lib/modules/*/; do
-  kernel_count=$(( kernel_count + 1 ))
-done
-
-if [ "${kernel_count}" -gt 1 ]; then
-  echo "NOTE: There are several versions of kernel's initramfs."
-  echo "      There is a possibility that you have multiple kernels installed in the image."
-  echo "      It is most ideal to have only 1 kernel, to make initramfs regeneration faster."
-fi
-
-# Set dracut log levels using temporary configuration file.
-# This avoids logging messages to the system journal, which can significantly
-# impact performance in the default configuration.
 temp_conf_file="$(mktemp '/etc/dracut.conf.d/zzz-loglevels-XXXXXXXXXX.conf')"
 cat >"${temp_conf_file}" <<'EOF'
 stdloglvl=4
@@ -91,11 +61,12 @@ kmsgloglvl=0
 fileloglvl=0
 EOF
 
-for kernel_path in /usr/lib/modules/*/; do
+for kernel_path in /usr/lib/modules/*/
+do
   kernel_path="${kernel_path%/}"
   initramfs_image="${kernel_path}/initramfs.img"
   qual_kernel=${kernel_path##*/}
-  echo "Starting initramfs regeneration for kernel version: ${qual_kernel}"
+  
   "${DRACUT}" \
     --kver "${qual_kernel}" \
     --force \
@@ -103,15 +74,12 @@ for kernel_path in /usr/lib/modules/*/; do
     --no-hostonly \
     --reproducible \
     "${initramfs_image}"
+    
   chmod 0600 "${initramfs_image}"
 done
 
 rm -- "${temp_conf_file}"
 
-# Bake in the HDMI 2.1 FRL enable karg. Without this, Harry Wentland's
-# FRL series stays gated off (DC_FRL_MASK / dcfeaturemask bit 0x400) and
-# HDMI 2.1 sinks fall back to TMDS. bootc applies kargs from this
-# directory on every deployment of the image.
 install -D -m 0644 /ctx/40-amdgpu-frl.toml /usr/lib/bootc/kargs.d/40-amdgpu-frl.toml
 
 ostree container commit
